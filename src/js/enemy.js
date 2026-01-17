5c2cef4abaa2692e31ec3b20e5c22fd5ff952fe2
Maksim Mayorov <maj-maksim@yandex.ru>
2026-01-17 11:40:34 +0300

    feat: add enemy

---
+++ src/js/enemy.js
@@ -0,0 +1,30 @@
+export class Enemy {
+    #x = 0;
+    #y = 0;
+    #width = 0;
+    #height = 0;
+    #speed = 0;
+
+    constructor(x, y, width, height, speed) {
+        this.#x = x;
+        this.#y = y;
+        this.#width = width;
+        this.#height = height;
+        this.#speed = speed;
+    }
+
+    update(gameField) {
+        this.#y += this.#speed;
+        if (this.#y > gameField.height + this.#height) this.#reset(gameField);
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = "red";
+        ctx.fillRect(this.#x, this.#y, this.#width, this.#height);
+    }
+
+    #reset(gameField) {
+        this.#x = Math.random() * (gameField.width - this.#width);
+        this.#y = -this.#height;
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import { Player } from "./player.js";
 import { InputHandler } from "./input.js";
+import { Enemy } from "./enemy.js";
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 320;
@@ -7,6 +8,7 @@ canvas.height = 480;
 let animationId = null;
 let player = null;
 let input = null;
+let enemy = null;
 const gameField = {
     width: canvas.width,
     height: canvas.height
@@ -21,16 +23,22 @@ function init() {
     const playerSpeed = 3;
     player = new Player(playerX, playerY, playerWidth, playerHeight, playerSpeed);
     input = new InputHandler(canvas);
+    const [enemyWidth, enemyHeight] = [20, 20];
+    const [enemyX, enemyY] = [Math.random() * (canvas.width - enemyWidth), -enemyHeight];
+    const enemySpeed = 2;
+    enemy = new Enemy(enemyX, enemyY, enemyWidth, enemyHeight, enemySpeed);
     requestAnimationFrame(gameLoop);
 }
 
 function update() {
     player.update(input.keys, gameField);
+    enemy.update(gameField);
 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     player.draw(ctx);
+    enemy.draw(ctx);
 }
 
 function gameLoop() {
